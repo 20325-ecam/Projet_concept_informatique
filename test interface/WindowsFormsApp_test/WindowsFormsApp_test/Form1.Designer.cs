7e0e0b9484bfd06001f2c01bb62788edfb441567
20325-ecam <75428443+20325-ecam@users.noreply.github.com>
2020-12-10 19:01:25 +0100

    -

---
+++ test interface/WindowsFormsApp_test/WindowsFormsApp_test/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WindowsFormsApp_test.presentation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.formsPlot1 = new ScottPlot.FormsPlot();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(201, 228);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // formsPlot1
+            // 
+            this.formsPlot1.Location = new System.Drawing.Point(534, 45);
+            this.formsPlot1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.formsPlot1.Name = "formsPlot1";
+            this.formsPlot1.Size = new System.Drawing.Size(1385, 359);
+            this.formsPlot1.TabIndex = 1;
+            this.formsPlot1.Load += new System.EventHandler(this.formsPlot1_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2098, 1464);
+            this.Controls.Add(this.formsPlot1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private ScottPlot.FormsPlot formsPlot1;
+    }
+}
---
+++ test interface/WindowsFormsApp_test/WindowsFormsApp_test/Form1.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ScottPlot;
+using ScottPlot.WinForms;
+
+namespace WindowsFormsApp_test.presentation
+{
+    public partial class Form1 : Form
+    {
+        double[] dataX = new double[] { 1 };
+        double[] dataY = new double[] { 1 };
+        int X = 0;
+        int Y = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        public void test()
+        {
+            formsPlot1.plt.PlotScatter(dataX, dataY);
+            formsPlot1.Render();
+        }
+
+        public void chaud()
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            X += 1;
+            Y += 5;
+            dataX = dataX.Concat(new double[] { X }).ToArray();
+            dataY = dataY.Concat(new double[] { Y }).ToArray();
+            test();
+        }
+
+        private void formsPlot1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
