e8797a5aa83e70b5ceacb5ac3adc357da3e490dd
20325-ecam <75428443+20325-ecam@users.noreply.github.com>
2020-12-20 13:08:57 +0100

    V7

---
+++ simulation_reseau_elec V7/test_live_graphe/Vente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simulation_reseau_elec
+{
+    public class Vente : Consommateur //classe qui permet de vensre le surplus de production énergétique
+    {
+        public double price;
+        public Vente(int max_conso, Market market) : base(max_conso)
+        {
+            this.price = market.Get_v_price();  //récupération du prix KWh du marché
+        }
+        public override double Get_vente(double trou_energie, Consommateur disipateur)
+        {
+            
+            if ((trou_energie - max_conso) > 0) //vente pas assez importante 
+            {
+                // appel methode disipateur ********************************************************
+                trou_energie -= max_conso;
+                disipateur.Get_dissip(trou_energie);
+                return trou_energie;
+            }
+            else  //vente a ecoule tous le surplus
+            {
+                return trou_energie;
+            }
+        }
+    }
+}
